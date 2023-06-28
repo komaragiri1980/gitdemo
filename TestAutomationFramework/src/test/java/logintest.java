aaae98635532f1fdbec8195c18a31e65747347a0
Mkomaragiri <Mkomaragiri@Sund71-PC-PC.sbc.local>
2023-06-28 12:30:44 -0500

    First commit

---
+++ TestAutomationFramework/src/test/java/logintest.java
@@ -0,0 +1,21 @@
+
+public class logintest {
+
+	
+	 public static void main(String[] args) {
+		 
+		 logintest lt= new logintest();
+		 
+		lt.login();
+		 
+	 }
+		
+	public void  login() {
+		
+		
+System.out.println("git practice");
+	
+	
+	
+}
+}
